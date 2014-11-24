2bcb4ecccdd492c8f8ee2fd2b19461286b82a313
Claudio Barbosa dos Santos <claudiobs11@gmail.com>
2014-11-24 12:15:57 -0200

    primeira versão da classe fatorial

---
+++ fatorial.java
@@ -0,0 +1,18 @@
+public class fatorial{
+   
+   public static void main(String [] args){
+      System.out.println(calcularFatorial(5));
+ 
+
+   } 
+
+   public static int (calcularFatorial(int n){
+
+    int resultado = 1;
+    for (int 1 =2; i<=n; I++)
+       resultado*=i;
+   }
+
+
+
+}
