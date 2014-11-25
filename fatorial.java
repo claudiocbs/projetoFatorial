b467a73ed4d6c1f39ae774b65c747af0a383d3bf
Claudio Barbosa dos Santos <claudiobs11@gmail.com>
2014-11-25 12:10:32 -0200

    criação de testeFatorial e refatoração 1 de fatorial

--- fatorial.java
+++ fatorial.java
@@ -1,12 +1,8 @@
 public class fatorial{
    
-   public static void main(String [] args){
-      System.out.println(calcularFatorial(5));
- 
 
-   } 
 
-   public static int (calcularFatorial(int n){
+   public int (calcularFatorial(int n){
 
     int resultado = 1;
     for (int 1 =2; i<=n; I++)
---
+++ testeFatorial.java
@@ -0,0 +1,13 @@
+public static class testefatorial{
+
+
+   public static void main(String [] args){
+     
+      System.out.println(new Fatorial().calcularFatorial(5));
+ 
+
+   } 
+
+
+
+}
